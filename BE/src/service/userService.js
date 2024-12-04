38cfa70780ffb3b7eb09a3e1f992066919c191d8
sansukai <lobaoduy2017@gmail.com>
2024-12-04 17:00:11 +0700

    test commit add

--- BE/src/service/userService.js
+++ BE/src/service/userService.js
@@ -1,12 +1,21 @@
 const bcrypt = require('bcryptjs');
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+import bluebird from 'bluebird';
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt',
-    password: "Duyngo123@"
-});
+
+
+const createConnect = async () => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'jwt',
+        password: "Duyngo123@",
+        Promise: bluebird,
+    });
+    return connection;
+}
+
+
 
 let hashPasswordService = (userPassword) => {
     let salt = bcrypt.genSaltSync(10);
@@ -15,36 +24,49 @@ let hashPasswordService = (userPassword) => {
     return hash;
 }
 
-let createNewUserService = (email, userName, password) => {
+let createNewUserService = async (email, userName, password) => {
     let hashPassword = hashPasswordService(password)
 
-    connection.query(
-        'INSERT INTO users (email,password,userName) VALUES (?,?,?)', [email, hashPassword, userName],
-        function (err, results, fields) {
-            if (err) {
-                console.log(err);
-            }
-            console.log(results);
-        }
-    );
+    try {
+        const connection = await createConnect();
+        const [rows, fields] = await connection.execute(
+            'INSERT INTO users (email,password,userName) VALUES (?,?,?)', [email, hashPassword, userName],
+        );
+    }
+
+    catch (e) {
+        console.log(e);
+    }
 }
 
-let getAllUsersService = () => {
-    connection.query(
-        "SELECT * FROM users",
-        function (err, results, fields) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(results)
-            }
-        }
-    )
+let getAllUsersService = async () => {
+    try {
+        const connection = await createConnect();
+        const [rows, fields] = await connection.execute(
+            "SELECT * FROM users"
+        );
+
+        return rows;
+    }
+    catch (e) {
+        console.log(e);
+    }
 
 
+}
+
+let deleteUserService = async (id) => {
+    try {
+        const connection = await createConnect();
+        const [rows, fields] = await connection.execute(
+            "SELECT * FROM users"
+        );
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = {
-    hashPasswordService, createNewUserService, getAllUsersService
+    hashPasswordService, createNewUserService, getAllUsersService, deleteUserService
 }
