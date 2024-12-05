5f922da381ad8e934b611b9a2ab7c87a3e9f366a
sansukai <lobaoduy2017@gmail.com>
2024-12-05 23:28:47 +0700

    create 3 files, update 3 files and move 1 file

--- BE/src/sever.js
+++ BE/src/sever.js
@@ -1,8 +1,7 @@
 import express from "express";
-import configViewEngine from "./configs/viewEngine";
+import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require('dotenv').config();
-
 const app = express();
 
 
@@ -19,6 +18,7 @@ app.use(bodyParser.json())
 initWebRoutes(app);
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
     console.log(">>> JWT Backend is running on the port = " + PORT);
 })
