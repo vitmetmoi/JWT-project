aae22bb15d34ae443c548472d542f0c86e4deafd
sansukai <lobaoduy2017@gmail.com>
2024-12-09 00:27:33 +0700

    delete 1 file and update 2 files

---
+++ BE/src/migrations/migrate-group-role.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Group_Role', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            groupId: {
+                type: Sequelize.INTEGER
+            },
+            roleId: {
+                type: Sequelize.INTEGER
+            },
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Groups_Role');
+    }
+};
