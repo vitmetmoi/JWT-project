4cfbc70bca4e1acec4ac959e08ee275feab361a1
sansukai <lobaoduy2017@gmail.com>
2024-12-16 23:39:16 +0700

    update

---
+++ FE/src/components/User/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Navigation/Nav';
+function HomePage(props) {
+    return (
+        <div>
+
+            userPage
+        </div>
+    );
+}
+
+export default HomePage;
