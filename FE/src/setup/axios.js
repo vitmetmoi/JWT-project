01acfc98df1df638ccad86e33217d5484bbcebc5
sansukai <lobaoduy2017@gmail.com>
2024-12-28 16:18:20 +0700

    update axios.js

--- FE/src/setup/axios.js
+++ FE/src/setup/axios.js
@@ -9,7 +9,7 @@ const instance = axios.create({
 
 instance.defaults.withCredentials = true;
 
-axios.interceptors.request.use(function (config) {
+instance.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
 }, function (error) {
@@ -17,7 +17,7 @@ axios.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
-axios.interceptors.response.use(function (response) {
+instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     console.log('res axios', response)
